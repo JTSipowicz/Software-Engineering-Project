ff47be86f9a844a01e472d38be36e67fc286cc6b
James Sipowicz III <55172879+JTSipowicz@users.noreply.github.com>
2020-04-25 21:42:02 -0500

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,36 @@
+package CurrencyExchange;
+
+public class Account {
+	private String userName;
+	private String password;
+	private String email;
+	public Account(String userName, String password, String email) {
+		super();
+		this.userName = userName;
+		this.password = password;
+		this.email = email;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	@Override
+	public String toString() {
+		return "Account [userName=" + userName + ", password=" + password + ", email=" + email + "]";
+	}
+	
+}
